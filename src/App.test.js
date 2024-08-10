8a3d1686486935d73cbe9f37e2db3f80666381b5
sarveshmahajan89 <sarveshmahajan89@gmail.com>
2024-08-10 16:26:11 +0530

    Restructuring app for the calculator requirements

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import StringCalculator from './StringCalculator';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<StringCalculator />);
+  const linkElement = screen.getByText(/String Calculator TDD Kata App/i);
   expect(linkElement).toBeInTheDocument();
 });
